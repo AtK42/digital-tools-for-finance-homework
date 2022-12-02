944db79d30ca9ab70ea6f2bbbc9e5d330557919d
AtK42 <aaron_7@live.de>
2022-12-02 16:09:11 +0100

    some further work on models, added NASDAQ and VIX

--- housing-price-index-us/src/data/get_FRED_data.py
+++ housing-price-index-us/src/data/get_FRED_data.py
@@ -9,10 +9,10 @@ from get_FRED_data_fct import *
 
 #test our function
 #series_id = ['HPIPONM226S']
-series_id_vec = ['HPIPONM226S', 'UNRATE', 'USACPIALLMINMEI', 'UMCSENT', 'PCU333120333120', 'T10YIEM', 'MORTGAGE30US', 'PI', 'AAA', 'MSACSR', 'POPTHM', 'HOUST', 'LFWA64TTUSM647S', 'FIXHAI', 'HOSSUPUSM673N']
+series_id_vec = ['CSUSHPISA', 'UNRATE', 'USACPIALLMINMEI', 'UMCSENT', 'PCU333120333120', 'T10YIEM', 'MORTGAGE30US', 'PI', 'AAA', 'MSACSR', 'POPTHM', 'HOUST', 'LFWA64TTUSM647S', 'NASDAQCOM', 'VIXCLS']
 frequency = 'm'
 observation_start = '2000-01-01'
-#observation_end = '2022-06-30'
+observation_end = '2022-06-30'
 api_key = os.environ['FRED_API_KEY']
 project_name = 'housing-price-index-us'
-fct_call_test = get_FRED_data_fct(series_id_vec, frequency, observation_start, api_key, project_name)
+fct_call_test = get_FRED_data_fct(series_id_vec, frequency, observation_start, api_key, project_name, observation_end)
